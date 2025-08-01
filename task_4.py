b5ecdadd442d30b0e62c4f4f2a2f8d2f17981fe5
Julia Zimina <krypina2009@yandex.ru>
2025-08-01 13:27:51 +0300

    Исправлена задача 4 спринта 2 v2.0

--- task_4.py
+++ task_4.py
@@ -28,14 +28,12 @@ class EmployeeSalary:
 
     # Создаем метод класса для расчета ЗП
     def salary(self):
-        print(
-            f"{self.name}, ваша заработная плата за {self.hours} часов составляет {self.hours * self.hourly_payment}. Расчетный лист выслали вам на почту {self.email}"
-        )
+        return f"{self.name}, ваша заработная плата за {self.hours} часов составляет {self.hours * self.hourly_payment}. Расчетный лист выслали вам на почту {self.email}"
 
 
 emp_1 = EmployeeSalary("Маша", 4, 1, "m@test.ru")
-emp_1.salary()
+print(emp_1.salary())
 emp_2 = EmployeeSalary.get_hours("Катя", rest_days=1, email="k@test.ru")
-emp_2.salary()
+print(emp_2.salary())
 emp_3 = EmployeeSalary.get_email("Алена", 6, 1)
-emp_3.salary()
+print(emp_3.salary())
